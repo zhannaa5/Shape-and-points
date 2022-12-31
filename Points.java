07d74f812ed9a8d5722f0dd21ad475e53cd59a1c
zhannaa5 <121726316+zhannaa5@users.noreply.github.com>
2022-12-31 21:56:36 +0600

    Create Points.java

---
+++ Points.java
@@ -0,0 +1,30 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        setX(x);
+        setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public static double findDistance(Point p){
+        int formula = p.x * p.x + p.y * p.y;
+        return Math.sqrt(formula);
+    }
+}
