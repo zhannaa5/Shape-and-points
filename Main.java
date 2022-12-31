970a27c54769a178f52a4b8ad36db62e5c8d363d
zhannaa5 <121726316+zhannaa5@users.noreply.github.com>
2022-12-31 21:56:09 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        AssignmentOne assignmentOne = new AssignmentOne("file1.txt");
+
+        System.out.println("Perimeter " + assignmentOne.shape.calculatePerimeter());
+        System.out.println("The longest side: "+ assignmentOne.shape.findLongestSide());
+        System.out.println("The average length: " + assignmentOne.shape.returnAverageLength());
+    }
+}
